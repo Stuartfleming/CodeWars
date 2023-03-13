a369ac7b9f7adf6a0f52b40c9e7c3e883412b106
Stuartfleming <119344054+Stuartfleming@users.noreply.github.com>
2023-03-12 19:36:39 -0500

    Add files via upload

---
+++ Adding_variables.js
@@ -0,0 +1,9 @@
+var laLigaGoals
+var championsLeagueGoals
+var copaDelReyGoals
+var totalGoals
+function adding (laLigaGoals, championsLeagueGoals, copaDelReyGoals ) {
+totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+ return totalGoals 
+}
+adding(43, 10, 5)
