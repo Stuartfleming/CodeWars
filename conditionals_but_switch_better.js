747c7709586985a9145840a22dae6c4e28f58774
Stuartfleming <119344054+Stuartfleming@users.noreply.github.com>
2023-04-01 08:47:39 -0500

    Add files via upload

---
+++ conditionals_but_switch_better.js
@@ -0,0 +1,22 @@
+function basicOp(operation, value1, value2)
+{ if(operation === '+'){ return value1 + value2} else if (operation === '-'){ return value1 - value2}
+ else if (operation === '/'){ return value1 / value2} else if (operation === '*'){ return value1 * value2}
+  
+}
+
+/*
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return 0;
+    }
+}
+*/
