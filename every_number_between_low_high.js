c2913c221f65a27fe3f79f006042b69b4b9d2761
Stuartfleming <119344054+Stuartfleming@users.noreply.github.com>
2023-05-12 11:59:40 -0500

    Add files via upload

---
+++ every_number_between_low_high.js
@@ -0,0 +1,12 @@
+function pipeFix(numbers){
+  var min = numbers[0];
+  var max = numbers[numbers.length - 1];
+  var array = [];
+  
+  for(var i = min; i<=max; i++)
+  {
+    array.push(i);
+  }
+  
+  return array;
+}
