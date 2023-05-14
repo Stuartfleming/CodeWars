23dedb0af072fceb6ec804a6cf0005070878eef3
Stuartfleming <119344054+Stuartfleming@users.noreply.github.com>
2023-05-14 08:52:11 -0500

    Add files via upload

---
+++ includes_in_string.js
@@ -0,0 +1,6 @@
+function chromosomeCheck(sperm) {
+  
+  if( sperm.includes('Y')) { return "Congratulations! You're going to have a son."
+                       } else { return "Congratulations! You're going to have a daughter."}
+  
+}
