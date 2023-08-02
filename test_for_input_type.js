25b5142dc1ce0fbe2813e98762533fd207bbe336
Stuartfleming <119344054+Stuartfleming@users.noreply.github.com>
2023-08-02 12:07:12 -0500

    Add files via upload

---
+++ test_for_input_type.js
@@ -0,0 +1,5 @@
+function cookie(x){
+  let name 
+  if (typeof  x == "string" ) { name = 'Zach'} else if ( typeof x == "number") { name  = 'Monica'} else { name = 'the dog'}
+  return `Who ate the last cookie? It was ${name}!`
+ }
