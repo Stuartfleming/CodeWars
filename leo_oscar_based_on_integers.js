ee5b428f8c377c1c31935b56424ace18fdd71498
Stuartfleming <119344054+Stuartfleming@users.noreply.github.com>
2023-08-05 11:49:25 -0500

    Add files via upload

---
+++ leo_oscar_based_on_integers.js
@@ -0,0 +1,7 @@
+function leo(oscar){
+  if (oscar > 88) {return "Leo got one already!"} 
+  else if ( oscar == 87 || oscar < 86) { return  "When will you give Leo an Oscar?"}
+  else if ( oscar == 88) {return "Leo finally won the oscar! Leo is happy"}
+  else { return "Not even for Wolf of wallstreet?!" }
+
+}
